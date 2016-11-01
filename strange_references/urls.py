27b2267d50f14fe997c7f92248e060fb1b68f91a
Brian Gunawan <brian.gunawan@gmail.com>
2016-11-01 19:40:03 +0000

    finished wireframe of login page which redirects to an authenticate view for processing

--- strange_references/urls.py
+++ strange_references/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'strange_references'
-urlpatterns = [
-	url(r'^$', views.login, name='login'),
-	url(r'sdf', views.authenticate, name='authenticate'),
+urlpatterns = [	
+	# url(r'dashboard', views.dashboard, name='dashboard'),
+	url(r'auth', views.authenticate, name='authenticate'),
+	url(r'^', views.login, name='login'),
+	# url(r'home', views.dashboard, name='dashboard'),
 ]
--- strange_references/views.py
+++ strange_references/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
+
+
+class User(object):
+	uname = ""
+	pwd = ""
+
+	def __init__(self, username, password):
+		self.uname = username
+		self.pwd = password
+
 
 
 def login(request):
@@ -11,4 +24,16 @@ def login(request):
 	return HttpResponse(template.render(context, request))
 
 def authenticate (request):
-	return HttpResponse("authenticating")
+	template = loader.get_template('strange_references/dashboard.html')
+
+	username = request.POST.get('uname')
+	password = request.POST.get('pwd')
+
+	user = User(username, password)
+
+	context = {
+		'user': user,
+	}
+	
+	return HttpResponse(template.render(context))
+
