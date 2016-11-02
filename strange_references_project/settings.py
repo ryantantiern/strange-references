67ac37f152832b3c94eefd31b1bb2f42e3f8c5ce
Wayne Tsui <tsuiwwwayne@hotmail.com>
2016-11-02 15:29:54 +0000

    Added references model and database configuration settings

--- strange_references_project/settings.py
+++ strange_references_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'strange_references_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'strange-references-db',
+        'USER': 'grouphuser',
+        'PASSWORD': 'grouphuser',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
