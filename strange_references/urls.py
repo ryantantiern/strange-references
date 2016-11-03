3e84cf21c56cb770c78775ca167007abff768a89
ben <ben.loh.15@ucl.ac.uk>
2016-11-03 00:47:07 +0000

    Change urls.py order

--- strange_references/urls.py
+++ strange_references/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
 	# url(r'dashboard', views.dashboard, name='dashboard'),
     url(r'^register$', views.register, name = 'register'),
 	url(r'^auth$', views.authenticate, name='authenticate'),
-	url(r'^', views.login, name='login'),
-	url(r'^hook$', views.hook, name='hook')
+	url(r'^hook$', views.hook, name='hook'),
+	url(r'^', views.login, name='login')
 	# url(r'home', views.dashboard, name='dashboard'),
 
 ]
