04b671373b497f262b5a88c43df5ffec41deadd0
Christopher Lau Min Yi <zcabmyl@ucl.ac.uk>
2016-11-03 19:30:35 +0000

    final changes to templates

--- strange_references/urls.py
+++ strange_references/urls.py
@@ -3,11 +3,9 @@ from . import views
 
 app_name = 'strange_references'
 urlpatterns = [
-    # url(r'dashboard', views.dashboard, name='dashboard'),
     url(r'^dashboard/new_reference$', views.add_form, name = 'new_reference'),
     url(r'^dashboard/delete_reference/(?P<reference_id>[0-9]+)$', views.delete_reference, name='delete_reference'),
     url(r'^dashboard/edit_reference/(?P<reference_id>[0-9]+)$', views.save_reference, name='edit_reference'),
-    # url(r'^dashboard/edit_reference/(?P<reference_id>[0-9]+)$', views.edit_form, name='edit_reference'),
     url(r'^dashboard/add_reference$', views.add_reference, name = 'add_reference'),
 	url(r'^dashboard$', views.dashboard, name = 'dashboard'),
     url(r'^register$', views.register, name = 'register'),
@@ -16,5 +14,4 @@ urlpatterns = [
     url(r'^register$', views.register, name = 'register'),
     url(r'^logout$', views.logout_account, name='logout'),
 	url(r'^', views.login, name='login'),
-	# url(r'home', views.dashboard, name='dashboard'),
 ]
