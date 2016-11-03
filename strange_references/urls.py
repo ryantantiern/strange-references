e9e6a7981c6d9e30bcdb1d2e91a101cb0c3d1d14
Wayne Tsui <tsuiwwwayne@hotmail.com>
2016-11-03 01:17:45 +0000

    Removed duplicate auth url

--- strange_references/urls.py
+++ strange_references/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     url(r'^login$', views.login, name='login'),
     url(r'^register$', views.register, name = 'register'),
     url(r'^logout$', views.logout_account, name='logout'),
-    url(r'^auth$', views.authenticate, name='authenticate'),
 	url(r'^dashboard$', views.dashboard, name = 'dashboard'),
 	url(r'^', views.login, name='login'),
 	# url(r'home', views.dashboard, name='dashboard'),
